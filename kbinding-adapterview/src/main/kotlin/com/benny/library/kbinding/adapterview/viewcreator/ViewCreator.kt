dc769ace507b3e15a844e5854e6613441b4553fb
ztc1997 <2455484588@qq.com>
2016-04-02 19:39:18 +0800

    ViewCreator: Added missing generic

--- kbinding-adapterview/src/main/kotlin/com/benny/library/kbinding/adapterview/viewcreator/ViewCreator.kt
+++ kbinding-adapterview/src/main/kotlin/com/benny/library/kbinding/adapterview/viewcreator/ViewCreator.kt
@@ -5,16 +5,16 @@ import android.view.ViewGroup
 import com.benny.library.autoadapter.utils.Func3
 import com.benny.library.autoadapter.viewcreator.IViewCreator
 import com.benny.library.autoadapter.viewcreator.ViewCreatorCollection
-import com.benny.library.kbinding.bind.BindingDisposer
 import com.benny.library.kbinding.adapterview.viewmodel.ItemViewModel
 import com.benny.library.kbinding.adapterview.viewmodel.MockItemViewModel
+import com.benny.library.kbinding.bind.BindingDisposer
 import com.benny.library.kbinding.view.BindingDisposerGenerator
 
 /**
  * Created by benny on 3/12/16.
  */
 
-open class ViewCreator(val bindingDisposer: BindingDisposer, val itemViewBinderComponent: ItemViewBinderComponent, val viewModelFactory: () -> ItemViewModel<*>) : IViewCreator<Any> {
+open class ViewCreator<T>(val bindingDisposer: BindingDisposer, val itemViewBinderComponent: ItemViewBinderComponent, val viewModelFactory: () -> ItemViewModel<T>) : IViewCreator<T> {
 
     override fun view(container: ViewGroup): View {
         val viewBinder = itemViewBinderComponent.createViewBinder(container.context, container)
@@ -24,8 +24,8 @@ open class ViewCreator(val bindingDisposer: BindingDisposer, val itemViewBinderC
         return viewBinder.view
     }
 
-    override fun viewTypeFor(data: Any?, position: Int, itemCount: Int): Int {
-        return  0
+    override fun viewTypeFor(data: T?, position: Int, itemCount: Int): Int {
+        return 0
     }
 
     override fun viewTypeCount(): Int {
@@ -33,10 +33,10 @@ open class ViewCreator(val bindingDisposer: BindingDisposer, val itemViewBinderC
     }
 }
 
-fun BindingDisposerGenerator.viewCreator(viewBinderComponent: ItemViewBinderComponent, viewModelFactory: () -> ItemViewModel<*>): IViewCreator<Any> = ViewCreator(bindingDisposer, viewBinderComponent, viewModelFactory)
-fun BindingDisposerGenerator.pagingViewCreator(loadingViewBinderComponent: ItemViewBinderComponent, itemViewBinderComponent: ItemViewBinderComponent, viewModelFactory: () -> ItemViewModel<*>): IViewCreator<Any> {
-    return ViewCreatorCollection.Builder<Any>()
+fun <T> BindingDisposerGenerator.viewCreator(viewBinderComponent: ItemViewBinderComponent, viewModelFactory: () -> ItemViewModel<T>) = ViewCreator(bindingDisposer, viewBinderComponent, viewModelFactory)
+fun <T> BindingDisposerGenerator.pagingViewCreator(loadingViewBinderComponent: ItemViewBinderComponent, itemViewBinderComponent: ItemViewBinderComponent, viewModelFactory: () -> ItemViewModel<T>): IViewCreator<T> {
+    return ViewCreatorCollection.Builder<T>()
             .addFilter(Func3 { data, position, itemCount -> data != null }, ViewCreator(bindingDisposer, itemViewBinderComponent, viewModelFactory))
-            .addFilter(Func3 { data, position, itemCount -> data == null && position == itemCount - 1 }, ViewCreator(bindingDisposer, loadingViewBinderComponent, { MockItemViewModel<Any>() }))
+            .addFilter(Func3 { data, position, itemCount -> data == null && position == itemCount - 1 }, ViewCreator(bindingDisposer, loadingViewBinderComponent, { MockItemViewModel<T>() }))
             .build()
 }
